7c31e139344bcbe23509778ef0b304a9d61380ed
Sadaqat Panhwer <panhwersadaqat22@gmail.com>
2020-05-26 00:52:09 +0500

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# ImageSlider
+This is a simple image slider example in java using card view and view-pager.
+
+![Image](screenshot.jpg)
---
+++ app/src/main/java/com/mdaftabalam/imageslider/SlidingModel.java
@@ -0,0 +1,14 @@
+package com.mdaftabalam.imageslider;
+
+class SlidingModel {
+
+    private int image_drawable;
+
+    int getImage_drawable() {
+        return image_drawable;
+    }
+
+    void setImage_drawable(int image_drawable) {
+        this.image_drawable = image_drawable;
+    }
+}
